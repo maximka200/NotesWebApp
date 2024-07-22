a3285faab6fa286a48b51b90de26a0a5c5f08e1a
maxim <sterben8912@gmail.com>
2024-07-22 15:16:56 +0500

    third comm

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,14 @@ import (
 	"log"
 
 	todo "github.com/maximka200/NotesWebApp"
+	handler "github.com/maximka200/NotesWebApp/pkg/handler"
 )
 
 func main() {
+	/// *gin.Engine реализует интерфейс http.Handler
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8800"); err != nil {
+	if err := srv.Run("8800", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running http server: %s", err)
 	}
 }
